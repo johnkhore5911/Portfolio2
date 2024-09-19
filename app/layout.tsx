80dcf04c34721c06c2e14ac83b63d52a7db8a4f5
Adrita12 <adubhattacharjee@gmail.com>
2024-09-19 12:26:10 +0530

    vercel analytics added

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
+import { Analytics } from "@vercel/analytics/react"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <StarsCanvas/>
         <Navbar/>
         {children}
+        <Analytics />
       </body>
     </html>
   );
